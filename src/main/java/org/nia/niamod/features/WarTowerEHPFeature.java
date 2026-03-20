6ac041eda55683b9dad08f318e9856413db92f56
d0cr67 <157885255+d0cr67@users.noreply.github.com>
2026-03-20 20:22:57 +0000

    war tower ehp

--- src/main/java/org/nia/niamod/features/WarTowerEHPFeature.java
+++ src/main/java/org/nia/niamod/features/WarTowerEHPFeature.java
@@ -1,6 +1,7 @@
 package org.nia.niamod.features;
 
 import net.minecraft.text.Text;
+import org.nia.niamod.config.NyahConfig;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,25 +9,35 @@ import java.util.regex.Pattern;
 public class WarTowerEHPFeature {
 
     private static final Pattern towerRegex = Pattern.compile(
-            "§4❤ ([0-9]+)§7 \\(§6([0-9.]+)%§7\\)"
+            "§3\\[([A-Za-z]{3,4})\\] §b([A-Za-z ]*)§7 - §4❤ ([0-9]+)§7 \\(§6([0-9.]+)%§7\\) - §c☠ ([0-9]+)-([0-9]+)§7 \\(§b([0-9]\\.[0-9]*)x§7\\)"
     );
 
     public Text replaceEHP(Text text) {
+        if (!NyahConfig.nyahConfigData.replaceTowerHP) return text;
         Matcher matcher = towerRegex.matcher(text.getString());
+        StringBuffer sb = new StringBuffer();
 
-        StringBuilder sb = new StringBuilder();
         while (matcher.find()) {
-            int number = Integer.parseInt(matcher.group(1));
-            double percent = Double.parseDouble(matcher.group(2)) / 100;
-            System.out.println(number);
-            System.out.println(percent);
-            double newValue = number / (1 - percent);
+            String tag = matcher.group(1);
+            String name = matcher.group(2);
+            int hp = Integer.parseInt(matcher.group(3));
+            double percent = Double.parseDouble(matcher.group(4));
+            int atckLow = Integer.parseInt(matcher.group(5));
+            int atckHigh = Integer.parseInt(matcher.group(6));
+            double speed = Double.parseDouble(matcher.group(7));
 
-            String replacement = "§4❤  " + newValue + "§7";
+            double newValue = hp * (1 - (percent / 100.0));
+
+            String replacement =
+                    "§3[" + tag + "] §b" + name + "§7 - §4❤ " + hp +
+                            "§7 (§6" + percent + "% → " + String.format("%.1f", newValue) + "§7)" +
+                            " - §c☠ " + atckLow + "-" + atckHigh +
+                            "§7 (§b" + speed + "x§7)";
 
             matcher.appendReplacement(sb, replacement);
         }
         matcher.appendTail(sb);
+
 
         return Text.of(sb.toString());
     }
