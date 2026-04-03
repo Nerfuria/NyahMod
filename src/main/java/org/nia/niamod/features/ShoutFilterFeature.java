e516bfa507006d02e71e1c81fed8ceb60c94d54c
d0cr67 <157885255+d0cr67@users.noreply.github.com>
2026-04-03 23:13:15 +0100

    fix

--- src/main/java/org/nia/niamod/features/ShoutFilterFeature.java
+++ src/main/java/org/nia/niamod/features/ShoutFilterFeature.java
@@ -1,14 +1,10 @@
 package org.nia.niamod.features;
 
-import com.wynntils.screens.territorymanagement.TerritoryManagementScreen;
 import org.nia.niamod.models.misc.Feature;
 
 public class ShoutFilterFeature extends Feature {
     @Override
     public void init() {
-
+        // TODO
     }
-
-    public String start = "\\uE060\\uDAFF\\uDFFF\\uE034\\uDAFF\\uDFFF\\uE044\\uDAFF\\uDFFF\\uE055\\uDAFF\\uDFFF\\uE062\\uDAFF\\uDFEC\\uE004\\uE014\\uE025\\uDB00\\uDC02 shouts: Today OUR Inxialot is";
-    public String start2 = "\\uE060\\uDAFF\\uDFFF\\uE034\\uDAFF\\uDFFF\\uE044\\uDAFF\\uDFFF\\uE051\\uDAFF\\uDFFF\\uE051\\uDAFF\\uDFFF\\uE062\\uDAFF\\uDFEA\\uE004\\uE014\\uE021\\uE021\\uDB00\\uDC02";
 }
