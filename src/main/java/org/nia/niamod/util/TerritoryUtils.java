99afb846761594d8fe5de6dacf21e303dba14293
Frederik <47461904+Freeder1k@users.noreply.github.com>
2026-04-06 11:14:50 +0200

    start writing more territory helper functions.

--- src/main/java/org/nia/niamod/util/TerritoryUtils.java
+++ src/main/java/org/nia/niamod/util/TerritoryUtils.java
@@ -2,7 +2,13 @@ package org.nia.niamod.util;
 
 import com.wynntils.models.territories.TerritoryInfo;
 import com.wynntils.models.territories.type.GuildResource;
+import com.wynntils.models.territories.type.TerritoryUpgrade;
 import com.wynntils.utils.type.CappedValue;
+import com.wynntils.utils.type.Pair;
+import org.nia.niamod.managers.FeatureManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TerritoryUtils {
     public static final GuildResource[] RESOURCES = GuildResource.values();
@@ -42,5 +48,72 @@ public class TerritoryUtils {
                 return TerritoryUtils.resStorageCostToLevel(storage.max());
         }
         return 0;
+    }
+
+    /**
+     * Get the resource cost of a territory.
+     **/
+    public static int getResCost(TerritoryInfo territoryInfo, GuildResource resource) {
+        int mapTick = FeatureManager.getResTickFeature().getMapTick();
+        if (mapTick <= 0)
+            return 0;
+
+        CappedValue storage = territoryInfo.getStorage(resource);
+        if (storage == null)
+            return 0;
+
+        int prod = territoryInfo.getGeneration(resource);
+
+        return (storage.current() * 60 * 60 / mapTick) - prod;
+    }
+
+    private static Pair<Integer, Integer> findUpgradeLevels(TerritoryUpgrade upgrade, TerritoryUpgrade bonus, int targetCost, boolean forceBonus) {
+        Pair<Integer, Integer> best = new Pair<>(0, 0);
+        long bestCost = 0;
+
+        for (int bonusLevel = forceBonus ? 1 : 0; bonusLevel < bonus.getLevels().length; bonusLevel++) {
+            long bonusCost = bonus.getLevels()[bonusLevel].cost();
+            for (int upgradeLevel = 0; upgradeLevel < upgrade.getLevels().length; upgradeLevel++) {
+                long upgradeCost = upgrade.getLevels()[upgradeLevel].cost();
+
+                long totalCost = upgradeCost + bonusCost;
+                if (totalCost <= targetCost + 50 && totalCost > bestCost) {
+                    best = new Pair<>(upgradeLevel, bonusLevel);
+                    bestCost = upgradeCost + bonusCost;
+                } else if (totalCost > targetCost + 50)
+                    break;
+            }
+        }
+        return best;
+    }
+
+    public static Map<TerritoryUpgrade, Integer> estimateUpgrades(TerritoryInfo territoryInfo) {
+        Map<TerritoryUpgrade, Integer> result = new HashMap<>();
+
+        if (territoryInfo.isHeadquarters())
+            return result;
+
+        boolean hasAuraVolley = territoryInfo.getDefences().getLevel() >= 3;
+
+        GuildResource[] resources = {GuildResource.ORE, GuildResource.CROPS, GuildResource.WOOD, GuildResource.FISH};
+        TerritoryUpgrade[] upgrades = {TerritoryUpgrade.DAMAGE, TerritoryUpgrade.ATTACK, TerritoryUpgrade.HEALTH, TerritoryUpgrade.DEFENCE};
+        TerritoryUpgrade[] bonuses = {TerritoryUpgrade.TOWER_VOLLEY, TerritoryUpgrade.TOWER_AURA, TerritoryUpgrade.STRONGER_MINIONS, TerritoryUpgrade.TOWER_MULTI_ATTACKS};
+
+        int[] otherCosts = {0, 0, 0, 0};
+        // TODO Emerald storage + prods
+
+        for (int i = 0; i < resources.length; i++) {
+            int cost = getResCost(territoryInfo, resources[i]);
+            cost -= otherCosts[i];
+
+            boolean forceAuraVolley = hasAuraVolley && (i == 0 || i == 1);
+
+            Pair<Integer, Integer> levels = findUpgradeLevels(upgrades[i], bonuses[i], cost, forceAuraVolley);
+
+            result.put(upgrades[i], levels.a());
+            result.put(bonuses[i], levels.b());
+        }
+
+        return result;
     }
 }
